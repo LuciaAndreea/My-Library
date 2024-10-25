647e6f610f5d4656c9cb5b699c9d31f0de805f6c
Lucia <luciaandreeaursu@yahoo.com>
2024-10-25 10:49:05 +0300

    create a function that displays a form to add a new book by the user when the button "NEW BOOK" is clicked

--- js.js
+++ js.js
@@ -34,3 +34,80 @@ function showBooks(){
     })
 }
 showBooks();
+
+//function to create a form which lets the user to enter a new book with all of it's attributes
+//the funtion will be called when the button "NEW BOOK" is clicked
+
+function createForm(){
+    const formContainer = document.getElementById("formContainer");
+    formContainer.innerHTML = ""; //clear the form container
+
+    //create the form labels and inputs
+    const form = document.createElement("form");
+
+    const titleLabel = document.createElement("label");
+    titleLabel.textContent = "Name of the book";
+
+    const titleInput = document.createElement("input");
+    titleInput.type = "text";
+    titleInput.name = "title";
+
+    const authorLabel = document.createElement("label");
+    authorLabel.textContent = "Author of th book";
+
+    const authorInput = document.createElement("input");
+    authorInput.type = "text";
+    authorInput.name = "author";
+
+    const pagesLabel = document.createElement("label");
+    pagesLabel.textContent = "Number of pages";
+
+    const pagesInput = document.createElement("input");
+    pagesInput.type = "number";
+    pagesInput.name = "pages";
+
+    const yearLabel = document.createElement("label");
+    yearLabel.textContent = "Year of publication";
+
+    const yearInput = document.createElement("input");
+    yearInput.type = "number";
+    yearInput.name = "year";
+
+    //create a button called "Add new book" which will add the book to the array
+    const btn = document.createElement("button");
+    btn.type = "submit";
+    btn.textContent = "Add new book";
+
+    //add the inputs, labels and the button to the form
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+    form.appendChild(authorLabel);
+    form.appendChild(authorInput);
+    form.appendChild(pagesLabel);
+    form.appendChild(pagesInput);
+    form.appendChild(yearLabel);
+    form.appendChild(yearInput);
+    form.appendChild(btn);
+
+    //add the form to the form container
+    formContainer.append(form);
+
+    //create the event which happens on the formm
+    form.addEventListener("submit", function(event){
+        event.preventDefault(); //prevent the submition because there is no server to submit this at
+        const newBook = {
+            title:titleInput.value,
+            author:authorInput.value,
+            pages:pagesInput.value,
+            year:yearInput.value
+        };
+
+        //add the new book to the existing library
+        books.push(newBook);
+        //show the updated list of books
+        showBooks();
+    });
+}
+
+const newBookButton = document.getElementById("new-book");
+newBookButton.addEventListener("click" , createForm);
